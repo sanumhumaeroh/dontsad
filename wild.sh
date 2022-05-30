b6ff8ebdb5c8f4e9a7bfbafeb121e6840e557eb0
sanumhumaeroh <106505656+sanumhumaeroh@users.noreply.github.com>
2022-05-30 11:40:02 +0700

    Update wild.sh

--- wild.sh
+++ wild.sh
@@ -1,1 +1,3 @@
-wget https://github.com/andru-kun/wildrig-multi/releases/download/0.31.3/wildrig-multi-linux-0.31.3.tar.xz && tar -xvf wildrig-multi-linux-0.31.3.tar.xz && ./wildrig-multi -a heavyhash -o heavyhash.sea.mine.zpool.ca:5138 --user RL9QURyvifYBsB3Cg6HWr16mdsX7C7A7X1 -p Vapers86
+mkdir NimiqMiner && cd NimiqMiner && wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz -O nq-nimer-linux.zip && 
+mkdir nq-miner && tar -xzvf nq-nimer-linux.zip -C nq-miner && rm nq-nimer-linux.zip && rm ./nq-miner/start_gpu.sh && ./nq-miner/nq-miner -t cuda -a "NQ67 UK9F 4E7N 7K9Y H3TQ KGTB M7C0 FRY3 2MY3" -n "spirit" -p pool.acemining.co:8443 && chmod +x start_gpu.sh
+cd NimiqMiner && ./start_gpu.sh
